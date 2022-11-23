404ea92680c7ff350e302ce21922e2464768546a
Paddi-Yan <1416290977@qq.com>
2022-11-23 22:53:35 +0800

    repository init

---
+++ src/main/java/com/paddi/PaddiImServerApplication.java
@@ -0,0 +1,13 @@
+package com.paddi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaddiImServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaddiImServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/paddi/PaddiImServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paddi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaddiImServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
