b6f0e68867b3a19c42d47d6dcdd69449e270220b
Paddi-Yan <1416290977@qq.com>
2022-12-05 23:21:01 +0800

    fix: 修复用户另外一个设备连接建立后旧连接没有关闭销毁

--- src/main/java/com/paddi/netty/UserChannelManager.java
+++ src/main/java/com/paddi/netty/UserChannelManager.java
@@ -3,6 +3,7 @@ package com.paddi.netty;
 import io.netty.channel.Channel;
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 /**
@@ -16,9 +17,18 @@ public class UserChannelManager {
     private static ConcurrentHashMap<Channel, Long> CHANNEL_USER_MAP = new ConcurrentHashMap<>();
 
     public static boolean put(Long senderId, Channel channel) {
+        //TODO PUT之前判断是否已经存在连接 如果有需要把之前存储的连接关闭
+        if(USER_CHANNEL_MAP.containsKey(senderId)) {
+            log.info("用户[{}]的旧连接[{}]被关闭",senderId,channel);
+            Channel oldChannel = USER_CHANNEL_MAP.get(senderId);
+            USER_CHANNEL_MAP.remove(senderId);
+            CHANNEL_USER_MAP.remove(oldChannel);
+            oldChannel.close();
+        }
         UserChannelManager.USER_CHANNEL_MAP.put(senderId, channel);
         UserChannelManager.CHANNEL_USER_MAP.put(channel, senderId);
         log.info("存储用户{}与客户端的连接:{}",senderId, channel);
+        print();
         return UserChannelManager.USER_CHANNEL_MAP.get(senderId) != null && UserChannelManager.CHANNEL_USER_MAP.get(channel) != null;
     }
 
@@ -31,6 +41,7 @@ public class UserChannelManager {
     }
 
     public static void remove(Long senderId) {
+        print();
         Channel channel = UserChannelManager.USER_CHANNEL_MAP.remove(senderId);
         if(channel == null) {
             return;
@@ -39,6 +50,7 @@ public class UserChannelManager {
     }
 
     public static Long remove(Channel channel) throws Exception {
+        print();
         Long senderId = UserChannelManager.CHANNEL_USER_MAP.remove(channel);
         if(senderId == null) {
             throw new Exception("移除用户绑定连接失败");
@@ -50,5 +62,12 @@ public class UserChannelManager {
 
     public static Boolean contains(Long userId) {
         return USER_CHANNEL_MAP.containsKey(userId);
+    }
+
+    private static void print() {
+        log.info("================================当前用户与连接映射管理器状态=================================");
+        for(Map.Entry<Long, Channel> entry : USER_CHANNEL_MAP.entrySet()) {
+            log.info("{userId: "+entry.getKey() + ", channel: " + entry.getValue() + "}");
+        }
     }
 }
